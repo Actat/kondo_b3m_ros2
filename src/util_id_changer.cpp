3397d3155b2761b887de52afb84f1ec68ffd73df
Actat <akio.toyosh@gmail.com>
2022-04-18 13:01:24 +0900

    debug

--- src/util_id_changer.cpp
+++ src/util_id_changer.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
-#include "b3m_port.hpp"
+#include "b3m_port.cpp"
 
-int main(int argc, char **argv) {
+int main() {
   std::cout << "Kondo B3M ID changer" << std::endl;
 
-  auto port_name = "/dev/ttyUSB0";
-  auto baud_rate = 1500000;
-  B3mPort *port  = new B3mPort(port_name, baud_rate);
+  std::string port_name = "/dev/ttyUSB0";
+  uint32_t baud_rate    = 1500000;
+  B3mPort *port         = new B3mPort(port_name, baud_rate);
 
-  std::cout << "port opened." << std::endl;
+  std::cout << "Port is opened." << std::endl;
+  std::cout << "Searching for motors. Please wait..." << std::endl;
 
   std::vector<uint8_t> motor_list;
   for (uint8_t i = 0; i < 0xFE; i++) {
@@ -19,54 +20,67 @@ int main(int argc, char **argv) {
     }
   }
 
-  std::size_t n_motor = motor_list.size();
+  int n_motor = motor_list.size();
   if (n_motor == 0) {
     std::cout << "No motor is found." << std::endl;
     std::cout << "abort." << std::endl;
     return 1;
   }
 
-  uint8_t *id_from = {0};
+  uint8_t id_from[1];
   if (n_motor == 1) {
     id_from[0] = motor_list[0];
-    std::cout << "One motor is found (id: " << id_from[0] << ")." << std::endl;
+    std::cout << "One motor is found (id: " << (int)id_from[0] << ")."
+              << std::endl;
   } else {
     std::cout << "Multiple motors are found." << std::endl;
     std::cout << "[";
     for (auto id : motor_list) {
-      std::cout << id << ", ";
+      std::cout << (int)id << ", ";
     }
     std::cout << "\b\b]" << std::endl;
+    int input;
     std::cout << "Select the motor to change the id. Enter the id: ";
-    std::cin >> id_from[0];
+    std::cin >> input;
+    id_from[0] = input;
   }
 
-  uint8_t *id_to = {0};
-  std::cout << "Enter the changed ID: ";
-  std::cin >> id_to[0];
-
-  std::cout << "The motor ID is changed from 0 to 1. Are you sure? [y/N]: ";
-  std::string buf;
-  std::cin >> buf;
-  if (buf != "y") {
+  uint8_t id_to[1];
+  int input;
+  std::cout << "Enter the ID to change to: ";
+  std::cin >> input;
+  if (input < 0 || input > 0xFE) {
+    std::cout << "ID must be in the range 0 - 254." << std::endl;
     std::cout << "abort." << std::endl;
     return 2;
+  }
+  id_to[0] = input;
+
+  std::cout << "The motor ID is changed from " << (int)id_from[0] << " to "
+            << (int)id_to[0] << ". Are you sure? [y/n]: ";
+  char buf;
+  std::cin >> buf;
+  if (buf != 'y') {
+    std::cout << "abort." << std::endl;
+    return 3;
   }
 
   if (!port->commandWrite(1, id_from, 1, id_to, 0x00)) {
     std::cout << "Write command failed." << std::endl;
-    return 3;
+    return 4;
   }
 
   if (!port->commandSave(1, id_to)) {
     std::cout << "Save command failed." << std::endl;
-    return 4;
+    return 5;
   }
 
   if (!port->commandReset(1, id_to)) {
     std::cout << "Reset command failed." << std::endl;
-    return 5;
+    return 6;
   }
+
+  std::cout << "Finished!" << std::endl;
 
   return 0;
 }
