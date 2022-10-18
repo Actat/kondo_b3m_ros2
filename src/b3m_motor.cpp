4411bdef043349d8de8f3519c37555b25d793012
Actat <akio.toyosh@gmail.com>
2022-10-18 09:30:35 +0900

    b3m_motor.cpp

---
+++ src/b3m_motor.cpp
@@ -0,0 +1,73 @@
+#include "b3m_motor.hpp"
+
+B3mMotor::B3mMotor(std::string json_string) {
+  size_t itr      = 0;
+  std::string key = "";
+  std::string val = "";
+
+  uint8_t id;
+  std::string name = "";
+  bool direction   = true;
+  double offset    = 0;
+
+  // JSON should be like this:
+  // {"id": 0, "name": "joint0", "direction": true, "offset": 0}
+  while (itr < json_string.size()) {
+    if (json_string.at(itr) == '{' || json_string.at(itr) == '}' ||
+        json_string.at(itr) == ' ' || json_string.at(itr) == ',') {
+      itr++;
+    } else if (json_string.at(itr) == '"') {
+      itr++;
+      while (json_string.at(itr) != '"') {
+        key += json_string.at(itr);
+        itr++;
+      }
+      itr++;  // the '"' following 'key'
+      while (json_string.at(itr) == ' ' || json_string.at(itr) == ':') {
+        itr++;
+      }
+
+      if (key == "id") {
+        while (json_string.at(itr) != ' ' && json_string.at(itr) != ',' &&
+               json_string.at(itr) != '}') {
+          val += json_string.at(itr);
+          itr++;
+        }
+        id = std::stoi(val);
+      } else if (key == "name") {
+        itr++;
+        while (json_string.at(itr) != '"' || json_string.at(itr - 1) == '\\') {
+          val += json_string.at(itr);
+          itr++;
+        }
+        itr++;
+        name = val;
+      } else if (key == "direction") {
+        while (json_string.at(itr) != ' ' && json_string.at(itr) != ',' &&
+               json_string.at(itr) != '}') {
+          val += json_string.at(itr);
+          itr++;
+        }
+        direction = (val == "true");
+      } else if (key == "offset") {
+        while (json_string.at(itr) != ' ' && json_string.at(itr) != ',' &&
+               json_string.at(itr) != '}') {
+          val += json_string.at(itr);
+          itr++;
+        }
+        offset = std::stod(val);
+      }
+      key = "";
+      val = "";
+    }
+  }
+
+  motor_id_        = id;
+  joint_direction_ = direction;
+  joint_offset_    = offset;
+  if (name.length() == 0) {
+    joint_name_ = std::to_string(id);
+  } else {
+    joint_name_ = name;
+  }
+}
